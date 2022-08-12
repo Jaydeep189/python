9be5cbee4f23fea99c8aa3ec73da76cc9279e545
Jaydeep <sjaydeep189@gmail.com>
2022-08-12 14:06:44 +0530

    Practical 4 added

--- 04_array.py
+++ 04_array.py
@@ -1,53 +1,84 @@
-if __name__ == "__main__":
-    program_flag = True
-    no_of_arrays = int(input("Enter the no of arrays: "))
-    dic_of_arrays = {}
-    def input_into_array():
-        dic_array = []
-        n = int(input("no of element in array : "))
-        for i in range(1, n+1):
-            ele = int(input())
-            dic_array.append(ele)
-        return dic_array
-    for i in range(1, no_of_arrays+1):
-        dic_of_arrays[i] = input_into_array();
-    while program_flag:
-        choice = int(input(
-            " "
-        ))
+main_array = []
 
-def add_into_array(dic_key, value):
-    dic_of_arrays[dic_key].append(value)
-    print(dic_of_arrays[dic_key])
+main_array_length = int(input("Enter the length of the main array"))
 
-def delete_from_array(dic_key, element):
-    try:
-        dic_of_arrays[dic_key].remove(element)
-    except:
-        print("Element is not present in the array")  
+for n in range(main_array_length):
+    main_array.append(int(input("Enter an element")))
 
-def update_in_array(dic_key, element):
-    try:
-        for i in dic_of_arrays[dic_key]:
-            if i == element:
-                index = dic_of_arrays[dic_key].index(i)
-                value = int(input("Enter a new value: "));
-                dic_of_arrays[dic_key][index] = value
-    except:
-        print("Element is not present in the array") 
+program_flag = True
 
-def search_in_array(dic_key, value):
-    flag = 0
-    for i in dic_of_arrays[dic_key]:
-        if i == value:
-            print("Element is present at position", dic_of_arrays[dic_key].index(i))     
-            flag = 1
-    if flag == 0:
-        print("Element is not present in the array") 
+def display_array(array):
+    for i in array:
+        print(i)
 
-def length_of_arrays():
-    for length in dic_of_arrays:
-        print("the length of the array " + str(length) + " is", len(dic_of_arrays[length]))
+def find_length():
+    print(len(main_array))
 
-def concate_list(dic_key_1, dic_key_2):
-    print("concated list \n", dic_of_arrays[dic_key_1] + dic_of_arrays[dic_key_2])
+def insert_element(element):
+    main_array.append(element)
+    display_array(main_array)
+
+def delte_element(element):
+    is_present = False
+    for n in main_array:
+        if n == element:
+            is_present = True
+            main_array.remove(element)
+    if is_present:
+        display_array(main_array)
+    else:
+        print("\nElement not present")
+
+def search_element(element):
+    is_present = False
+    for n in main_array:
+        if n == element:
+            is_present = True
+            print("The element is at index", main_array.index(n))
+    if is_present:
+        display_array(main_array)
+    else:
+        print("\nElement not present")    
+
+def update_element(element, value):
+    is_present = False
+    for n in main_array:
+        if n == element:
+            is_present = True
+            main_array[main_array.index(n)] = value
+    if is_present:
+        display_array(main_array)
+    else:
+        print("\nElement not present")
+
+def concate(array):
+    concated_array = main_array + array
+    display_array(concated_array)
+
+while(program_flag):
+    choice = int(input(
+        "1. Find Length\n2. Insert Element\n3. Delete Element\n4. Search Element\n5. Update Element\n6. Concate\n-1. Exit\n"
+    ))
+    if choice == -1:
+        program_flag = False
+    elif choice == 1:
+        find_length()
+    elif choice == 2:
+        value_2 = int(input("Enter a new Element"))
+        insert_element(value_2)
+    elif choice == 3:
+        value_3 = int(input("Enter a element to delete"))
+        delte_element(value_3)
+    elif choice == 4:
+        value_4 = int(input("Search an element in the list: "))
+        search_element(value_4)
+    elif choice == 5:
+        value_5 = int(input("Enter the value you want to update: "))
+        value_5_new = int(input("Enter the new value to replace"))
+        update_element(value_5, value_5_new)
+    elif choice == 6:
+        value_6_len = int(input("Enter the length of the new array: "))
+        new_array = []
+        for i in range(value_6_len):
+            new_array.append(int(input("Enter an element")))
+        concate(new_array)
